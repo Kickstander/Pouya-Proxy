2baebe2be01265210d9ea052a34a310b14f90bb3
Keiran <keirans@gmail.com>
2018-11-06 11:38:30 -0800

    add a route to serve up html from specific campaign routes

--- server/server.js
+++ server/server.js
@@ -4,9 +4,13 @@ const path = require('path');
 const port = 3000;
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, '../public/')));
+app.get(express.static(path.resolve(__dirname, '../public/')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.get('/:campaignId', (req, res) => { // handle requests for a given campaign's stats
+  res.status(200).sendFile(path.resolve(__dirname, '../public/index.html'));
+});
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
